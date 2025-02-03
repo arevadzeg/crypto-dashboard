d508d087418355387a0b99ffb2b323bee84dc0a8
arevadzeg <gioarevadze97@gmail.com>
2025-02-03 09:25:15 +0400

    test

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router';
-import styles from './header.module.scss';
+// import React from 'react';
+// import { Link } from 'react-router';
+// import styles from './header.module.scss';
 
 
-const HEADER_LIST = [
-    { label: "Dashboard", link: "/" },
-    { label: "Convert", link: "/convert/btc/usdt" },
-    { label: "Price", link: "/price/bitcoin" }
+// const HEADER_LIST = [
+//     { label: "Dashboard", link: "/" },
+//     { label: "Convert", link: "/convert/btc/usdt" },
+//     { label: "Price", link: "/price/bitcoin" }
 
 
-]
+// ]
 
-const Header: React.FC = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link to="/" className={styles.logoLink}>
-                    <img src="/src/assets/png/binanceLogo.png" alt="Binance Logo" className={styles.logoImage} />
-                </Link>
-            </div>
-            <nav>
-                <ul className={styles.navList}>
-                    {HEADER_LIST.map((item) => <li key={item.link} >
-                        <Link to={item.link} className={styles.navLink}>
-                            {item.label}
-                        </Link>
-                    </li>)}
-                </ul>
-            </nav>
-        </header>
-    );
-};
+const Header = () => {
+
+    return <div>asd</div>
+
+}
+
+// const Header = () => {
+//     return (
+//         <header className={styles.header}>
+//             <div className={styles.logo}>
+//                 <Link to="/" className={styles.logoLink}>
+//                     <img src="/src/assets/png/binanceLogo.png" alt="Binance Logo" className={styles.logoImage} />
+//                 </Link>
+//             </div>
+//             <nav>
+//                 <ul className={styles.navList}>
+//                     {HEADER_LIST.map((item) => <li key={item.link} >
+//                         <Link to={item.link} className={styles.navLink}>
+//                             {item.label}
+//                         </Link>
+//                     </li>)}
+//                 </ul>
+//             </nav>
+//         </header>
+//     );
+// };
 
 export default Header;
